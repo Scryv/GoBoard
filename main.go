6844e1807125a76b4a0a30af3ec1cc4ca4f25b0c
scryv <scryvcor@gmail.com>
2026-06-06 14:16:26 +0200

    Added auth and root Api points with templates folder for html/htmx docus together with there respective images and css

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	_"fmt"
-	"net/http"
+	_ "fmt"
+	"html/template"
 	"log"
+	"net/http"
 )
 
-
-func test(w http.ResponseWriter, r *http.Request) {
-	
-}
-
-
-func main(){
+func main() {
 
 	router := http.NewServeMux()
-	router.HandleFunc("/test", test)
+
+	router.HandleFunc("/{$}", handleRoot)
+	router.HandleFunc("/auth/login", handleLogin)
+	router.HandleFunc("/auth/signup", handleSignup)
 
 	log.Println("Listning")
 	err := http.ListenAndServe(":3000", router)
@@ -24,3 +22,17 @@ func main(){
 	}
 }
 
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	index := template.Must(template.ParseFiles("templates/index.html"))
+	index.Execute(w, nil)
+}
+
+func handleLogin(w http.ResponseWriter, _ *http.Request) {
+	login := template.Must(template.ParseFiles("templates/login.html"))
+	login.Execute(w, nil)
+}
+
+func handleSignup(w http.ResponseWriter, _ *http.Request) {
+	signup := template.Must(template.ParseFiles("templates/signup.html"))
+	signup.Execute(w, nil)
+}
